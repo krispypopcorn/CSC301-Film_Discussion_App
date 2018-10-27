53d5750a7b806980c3c7d309af088f00f42e8b4a
Faiyaz Huq <faiyaz.huq@mail.utoronto.ca>
2018-10-27 16:31:56 -0400

    Polished Chart Data, more data can be added/modified

--- Faiyaz/admin.js
+++ Faiyaz/admin.js
@@ -1,8 +1,9 @@
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChartView);
+google.charts.setOnLoadCallback(drawChartUser);
 
 // Draw the chart and set the chart values
-function drawChart() {
+function drawChartView() {
   var data = google.visualization.arrayToDataTable([
   ['Genre', 'Hours per Day'],
   ['Horror', 45],
@@ -13,9 +14,42 @@ function drawChart() {
 ]);
 
   // Optional; add a title and set the width and height of the chart
-  var options = {'title':'Average Viewing Genre', 'width':550, 'height':400};
+  var options = {'width':500,
+                'legend':'left',
+                'titleTextStyle': {'color': '#FAFAFA', 'fontSize': 20},
+                'title': 'Average Viewing Genre',
+                'height':400,
+                'is3D': true,
+                'backgroundColor': '#171718', 
+                'legendTextStyle': { 'color': '#FAFAFA' }          
+              };
 
   // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  var chart = new google.visualization.PieChart(document.getElementById('piechartV'));
+  chart.draw(data, options);
+}
+
+function drawChartUser() {
+  var data = google.visualization.arrayToDataTable([
+  ['Username', 'Post'],
+  ['Jennifer', 21],
+  ['Faiyaz',54],
+  ['Jane', 12],
+  ['Fionna', 68]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'width':500,
+                'legend':'left',
+                'titleTextStyle': {'color': '#FAFAFA', 'fontSize': 20},
+                'title': 'Top Contributers',
+                'height':400,
+                'is3D': true,
+                'backgroundColor': '#171718', 
+                'legendTextStyle': { 'color': '#FAFAFA' }          
+              };
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechartU'));
   chart.draw(data, options);
 }
