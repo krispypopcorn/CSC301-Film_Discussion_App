6c05a10ec1d29ad868628d6fcbe5a13787aaa2cc
FernandaPalacios <ferpalacios.a@gmail.com>
2018-11-06 14:00:16 -0500

    change valid credentials and handle case with invalid credentials

--- Login/index.js
+++ Login/index.js
@@ -1,27 +1,42 @@
 "use strict"
 
-$(".login_btn").click(checkPremission);
+$("#login_btn").click(checkPremission);
+
+const username = $('#username');
+const password = $('#password');
+let invalid_msg_on_screen = false
 
 function checkPremission(e){
     e.preventDefault()
 
-    const userName = $('#username').val();
-    const passWord = $('#password').val();
-
-    if (userName=="MyUserName" && passWord=="password"){
+    if (username.val()=="james" && password.val()=="jamespassword"){
         let queryString = "?para1=user"
         window.location.href = "../Homepage/homepage.html" + queryString;
     }
-    else if (userName=="AdminUserName" && passWord=="password"){
+    else if (username.val()=="admin" && password.val()=="adminpassword"){
         let queryString = "?para1=admin"
         window.location.href = "../AdminDash/admin.html" + queryString;
     }
+
     else{
-        notUser();
+        if(!invalid_msg_on_screen){
+            show_invalid_msg()
+        }    
     }
+    clear();
 }
 
-function notUser(){
-    
+function clear(){
+    username.val('');
+    password.val('');
+}
+
+function show_invalid_msg(){
+    const invalid_msg = document.createElement('div')
+    invalid_msg.append(document.createTextNode('Invalid username or password'))
+    invalid_msg.style.color = 'white';
+    $('#login_btn').before(invalid_msg)
+    invalid_msg_on_screen = true;
+
 }
 
