7c3f8e9be00f1edd03348532b26f177f553b369b
Micari <qikadate@aliyun.com>
2018-11-06 12:15:21 -0500

    Added permission checking

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -1,16 +1,28 @@
 "use strict"
 
+/*-------------Premission check-------------*/
+/*----get premission from previous page----*/
+/*------should be done through server-----*/
 let premission = "none";
 
-// get premission from login page
-// we should pull this from server
 let queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
-queryString = queryString.split("&")[0];
+queryString = queryString.split("?")[0];
 
 const index = queryString.indexOf("=");
 if(index!=-1){premission = queryString.substring(index+1);}
 
 if(premission=="none"){
    window.location.href = "../Login/index.html";
+} else if(premission=="user"){
+  $("#adminLink").hide();
 }
+/*----Premission check----*/
+
+
+$("#Latest .column").on('click', function(event) {window.location.href = "../DiscussionPage/discussion_topic_page.html" + "?para1="+ premission;});
+$("#MostPopular .column").on('click', function(event) {window.location.href = "../DiscussionPage/discussion_topic_page.html" + "?para1="+ premission;});
+$("#Movie .column").on('click', function(event) {window.location.href = "../MoviePage/movie_page.html" + "?para1="+ premission;});
+$("#homeLink").on('click', function(event) {window.location.href = "homepage.html" + "?para1="+ premission;});
+$("#adminLink").on('click', function(event) {window.location.href = "../AdminDash/admin.html" + "?para1="+ premission;});
+$("#signOut").on('click', function(event) {window.location.href = "../Login/index.html";});
--- MoviePage/movie_page.js
+++ MoviePage/movie_page.js
@@ -1,5 +1,24 @@
 "use strict"
 
+/*-------------Premission check-------------*/
+/*----get premission from previous page----*/
+/*------should be done through server-----*/
+let premission = "none";
+
+let queryString = decodeURIComponent(window.location.search);
+queryString = queryString.substring(1);
+queryString = queryString.split("?")[0];
+
+const index = queryString.indexOf("=");
+if(index!=-1){premission = queryString.substring(index+1);}
+
+if(premission=="none"){
+   window.location.href = "../Login/index.html";
+} else if(premission=="user"){
+  $("#adminLink").hide();
+}
+/*----Premission check----*/
+
 // number of total discussions under a movie
 // we should pull this from server
 let numberOfDiscusstions = 4;
@@ -66,6 +85,8 @@ $("#searchTerm").keyup(function(event) {
 $("#cleanSearch").click(restoreDiscussion);
 $(".previous").click(loadPreviousPage);
 $(".next").click(loadNextPage);
+$("#homeLink").on('click', function(event) {window.location.href = "../Homepage/homepage.html" + "?para1="+ premission;});
+$("#adminLink").on('click', function(event) {window.location.href = "../AdminDash/admin.html" + "?para1="+ premission;});
 
 function addNewDiscussion(e) {
 
