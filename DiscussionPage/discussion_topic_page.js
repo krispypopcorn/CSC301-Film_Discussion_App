02277dbc678e7bfa0ec2936a7db275f92b4e67c2
Ishaan Nagi <ghost@Ishaans-MacBook-Pro.local>
2018-11-08 10:44:19 -0500

    Create New post

--- DiscussionPage/discussion_topic_page.js
+++ DiscussionPage/discussion_topic_page.js
@@ -39,6 +39,10 @@ $("#bannerText").on('click', function(event) {window.location.href = "../MoviePa
 /*----navigate to another page----*/
 
 
+$("#replyPost").on('click', createReply);
+
+
+
 function fillDiscussionPost(){
 
 	//accessing Discussion Post elements (img, title, text) 
@@ -73,6 +77,117 @@ function replyToPost(postToReplyTo){
 
 
 }
+
+
+
+
+function createPost(){
+
+	let text = document.getElementById('postText');
+
+	let post = document.createElement('div');
+	post.className("card reply");
+	innerDiv1 = document.createElement('div');
+	innerDiv1.className("row no-gutters");
+	innerDiv2 = document.createElement('div');
+	innerDiv2.className("col");
+
+
+
+
+
+
+	return post;
+
+}
+
+// <div class="card border-dark">
+//         <div class="row no-gutters">
+//             <div class="col">
+//                 <div class="card-block px-2">
+//                     <p class="card-text">Galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum</p>
+
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="row card-footer w-100 text-muted">
+//             <p class='col-sm-2'>Post by <a id="yellow" class='usn'>USERNAME</a></p>
+//             <a href="#" id='reply' class="btn btn-primary col-sm-1 offset-sm-9"><strong>Reply</strong></a>
+//         </div>
+//     </div>
+
+
+
+function createReply(e){
+
+	let comments = document.getElementById("comments");
+
+	e.preventDefault();
+	let text = document.getElementById("postText").value;
+
+
+	let reply = document.createElement("div");
+	reply.classList.add("card");
+	reply.classList.add("border-dark");
+
+	let row = document.createElement("div");
+	row.classList.add("row"); 
+	row.classList.add("no-gutters");
+
+	let col = document.createElement("div");
+	col.classList.add("col");
+
+	let card_block = document.createElement("div");
+	card_block.classList.add("card-block"); 
+	card_block.classList.add("px-2");
+
+	let card_text = document.createElement("p");
+	card_text.classList.add("card-text");
+	let in_text = document.createTextNode(text);
+	
+
+	card_text.append(in_text);
+	card_block.append(card_text);
+	col.append(card_block);
+
+
+	let row_footer = document.createElement('div');
+	row_footer.classList.add("row");
+	row_footer.classList.add("card-footer");
+	row_footer.classList.add("w-100");
+	row_footer.classList.add("text-muted");
+
+	let footer_text_div = document.createElement("p");
+	let footer_text = document.createTextNode("Post by USERNAME");
+	footer_text_div.append(footer_text)
+
+
+	let reply_footer_btn = document.createElement('a');
+	reply_footer_btn.classList.add("btn");
+	reply_footer_btn.classList.add("btn-primary");
+	reply_footer_btn.classList.add("col-sm-1");
+	reply_footer_btn.classList.add("offset-sm-9");
+
+
+	let strong = document.createElement('strong');
+	let button_text = document.createTextNode("Reply");
+	strong.append(button_text);
+	reply_footer_btn.append(strong)
+
+	
+	row_footer.append(footer_text_div)
+	row_footer.append(reply_footer_btn)
+
+
+
+	row.append(col);
+
+	reply.append(row);
+	reply.append(row_footer)
+
+	comments.append(reply);
+}
+
 
 
 
