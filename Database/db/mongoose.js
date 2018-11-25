b97d401fa0002c47862728c3bc7558aac094a771
ZIYING ZHOU <Micari.zhou.utoronto.mail.ca>
2018-11-25 06:34:49 -0600

    deleting database

--- Database/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-// connect to our database
-mongoose.connect('mongodb://localhost:27017/ConspireView', { useNewUrlParser: true});
-
-module.exports = { mongoose }
