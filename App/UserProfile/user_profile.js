08cf63d6e8fec4092aa0d6c71cdbb1a8d28b93eb
FernandaPalacios <ferpalacios.a@gmail.com>
2018-12-05 12:13:19 -0500

    display discussions in users profile

--- App/UserProfile/user_profile.js
+++ App/UserProfile/user_profile.js
@@ -1,61 +1,8 @@
 "use strict"
 
-// number of total discussions under a movie
-// we should pull this from server
-let numberOfDiscusstions = 6;
-
-//array of discussions posted by user
-//we should pull this from server
-const discussions = [];
-
-//store search result
-let search = [];
-
-//indicate which section we are in right now
-//0 for discussion, 1 for comments, 2 for replies
-let view = 0;
-
-//1 if in search mode, 0 otherwise
-let searchMode = 0;
-
-//store current page
-let currentPage = 1;
 
 checkUserClass()
 setUserIcon()
-
-class Discussion {
-   constructor(title, author, content) {
-       //read from user input or pull from server
-       this.title = title;
-       this.author = author;
-       this.content = content;
-       this.thumbsUp = 130;
- 
-       //user can upload pic, hard code source link for now
-       this.image = '../Pictures/iron_man.jpg'
-   }
-}
-
-class User {
-   constructor(username, password) {
-       this.username = username;
-       this.password = password;
-   }
-}
-
-// Dummy user
-const DummyUser = new User("Dummy", "123")
-
-const DummyText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-
-discussions.push(new Discussion("Discussion Topic", DummyUser, DummyText));
-discussions.push(new Discussion("Discussion Topic", DummyUser, DummyText));
-discussions.push(new Discussion("Discussion Topic", DummyUser, DummyText));
-discussions.push(new Discussion("Discussion Topic", DummyUser, DummyText));
-discussions.push(new Discussion("Discussion Topic", DummyUser, DummyText));
-discussions.push(new Discussion("Discussion Topic", DummyUser, DummyText));
-
 
 /*-------------Add Event-listener-------------*/
 $("#profilePic").on('click', function(event) {window.location.href = "../UserProfile/user_profile.html";});
@@ -65,36 +12,14 @@ $("#signOut").on('click', function(event) {window.location.href = "../Login/inde
 $(".card-title").on('click', function(event) {window.location.href = "../DiscussionPage/discussion_topic_page.html";});
 $(".previous").on('click',loadPreviousPage);
 $(".next").on('click',loadNextPage);
-$('#DiscussionTopics').click(display_discussion_topics)
 $('#SaveButton').click(tryModifyPassword)
 
 /*-------------Add Event-listener-------------*/
 
-
+//store current page
+let currentPage = 1;
 
 const user = getUser()
-
-const html_categories = $('#Categories').children()
-
-var categories = {"discussion_topics":0}
-
-display_category(categories.discussion_topics)
-
-
-function displayUser(user){
-  const username = document.querySelector('#currUsername');
-  username.innerText = user.username
-  // you have the icon url in the user obj
-  $('#userPic').attr('src',user.icon)
-
-  fetch('/currentUserDiscussions').then((result)=>{
-    return result.json()
-  }).then((discussions)=>{
-    console.log(discussions);
-  }).catch((error)=>{
-    console.log(error);
-  })
-}
 
 function getUser(){
   const isCurrentUser = getCookie('isCurrentUser');
@@ -121,9 +46,38 @@ function getUser(){
 }
 
 
-function display_discussion_topics(e){
-	e.preventDefault();
-	display_category(categories.discussion_topics)
+function displayUser(user){
+  const username = document.querySelector('#currUsername');
+  username.innerText = user.username
+  // you have the icon url in the user obj
+  $('#userPic').attr('src',user.icon)
+
+  fetch('/currentUserDiscussions').then((result)=>{
+    return result.json()
+  }).then((discussions)=>{
+    displayDiscussions(discussions)
+  }).catch((error)=>{
+    console.log(error);
+  })
+}
+
+
+
+function displayDiscussions(discussionList) {
+
+   let i;
+   const targetList = [];
+   let newPost;
+   for (i = 0; i < discussionList.length && i < 4; i++) {
+       newPost = createDiscussion(discussionList[i]);
+       targetList.push(newPost);
+   }
+
+   $('#postsContainer .card').remove();
+
+   for (i = 0; i < targetList.length; i++) {
+       $("#postsContainer").append(targetList[i]);
+   }
 }
 
 
@@ -131,6 +85,8 @@ function display_discussion_topics(e){
 // Helper function
 // Creates a discussion div based on given discussion object
 function createDiscussion(discussion) {
+
+  console.log(discussion);
 
    let newPost = $(".card:first").clone();
    const target = newPost.children().children();
@@ -143,12 +99,11 @@ function createDiscussion(discussion) {
    newTitle.on('click',function(event) {window.location.href = "../DiscussionPage/discussion_topic_page.html" + "?para1="+ permission;});
 
    img.attr('src',discussion.image);
-   text.html(discussion.content);
-   if(view==0){newTitle.html(discussion.title);}
-   else if (view==1){newTitle.html("Comment");}
+   text.html(discussion.discussion_content);
+   newTitle.html(discussion.title);
    
    
-   upVote.html(discussion.thumbsUp.toString());
+   upVote.html(discussion.likes.toString());
 
    return newPost;
 }
@@ -265,42 +220,3 @@ function tryModifyPassword(e){
 /*-------------------------------------------------------*/
 /*Dom function below*/
 /*-------------------------------------------------------*/
-
-function addMultiplyDiscussion(discussionList) {
-   let i;
-   const targetList = [];
-   let newPost;
-   for (i = 0; i < discussionList.length && i < 4; i++) {
-       newPost = createDiscussion(discussionList[i]);
-       targetList.push(newPost);
-   }
-
-   $('#postsContainer .card').remove();
-
-   for (i = 0; i < targetList.length; i++) {
-       $("#postsContainer").append(targetList[i]);
-   }
-}
-
-function display_category(index){
-	if(0 <= index && index <= html_categories.length - 1){
-		html_categories[index].style.color = '#3366cc';
-		for(let i = 0; i < html_categories.length; i++){
-			if(i!== index){
-				html_categories[i].style.color = 'white';
-			}
-		}
-	}
-
-	// we should use a server call here to get current user
-	//current user holds different lists
-	//ex: discussion list, comments list
-	//useing hardcode list here for demo
-	if(index == 0){
-	  currentPage=1;
-	  view=0;
-		addMultiplyDiscussion(discussions)
-	}
-	
-}
-
